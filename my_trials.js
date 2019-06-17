aa47ccf943f456723fbe52325ced21c9337e7d76
Eloisa Furnaguera <eloisafurnaguera@MacBook-Air.local>
2019-06-16 19:18:17 -0700

    Initial Commit

--- my_trials.js
+++ my_trials.js
@@ -2,7 +2,6 @@
 // PART 1
 // Account information:
 
-
 let accountHolder = "Balloonicorn";
 const accountNumber = "123456";
 let businessName = "Balloonicorn's Cupcake Shop";
@@ -12,91 +11,37 @@ const addresses = ["123 Main Street", "683 Sutter Street", "1600 Pennsylvania Av
 const phoneNumbers = new Map();
 
 // Add some phone numbers to our map
-        phoneNumbers.set('home', '510-867-5309');
-        phoneNumbers.set('mobile', '415-555-1212');
-        phoneNumbers.set('business', '415-123-4567');
 
 
 
 // ///////////////////////////////////////////////////////
 // Create User Info Display:
-function printUserInfo(accountHolder, accountNumber, businessName){
 
 // Add function to print account information 
-console.log(`Account Holder: ${accountHolder}`);
+
 
 // Add function to print all addresses, including a header
 
-console.log(`Account Number: ${accountNumber}`);
 
 // Add function to print phone types and numbers
-
-console.log(`Business Address: ${businessName}`);
-
-}
-
-// console.log(printUserInfo('gaga', 646446, 'yeyeeh'))
-
-const bisnessPhoneNumbers = new Map;
-
-    
-
-
-
-function showPhoneNums(phones){
-
-    for (let[phone, number] of phoneNumbers){
-        console.log(`${phone}, ${number}`);
-}
-  
-    }
-// showPhoneNums()
-
 
 
 // ///////////////////////////////////////////////////////
 // Transactions:
 
 // Create an empty map of transactions
-const transactions = new Map ()
-
 
 
 // Add function to add transactions
-function addTrans (date, transVal){
 
-    transactions.set(date, transVal);
-
-}
 
 // Use the function to add transactions
 
-addTrans ('May-2', -500)
-addTrans ('May-13', 1200)
-addTrans ('May-15', -100)
-addTrans ('May-21', -359)
-addTrans ('May-29', 2200)
-
-// console.log(transactions)
 
 // Add function to show balance status
 
-function showBalance(Map){
-
-    
-}
-
-showBalance(transactions)
 
 // Add function to show transactions
-
-function showTran(Map){
-    
-
-    console.log (Map)
-}
-
-showBalance(transactions)
 
 
 // ///////////////////////////////////////////////////////
